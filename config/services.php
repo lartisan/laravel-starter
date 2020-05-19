61a89e06757060284c012c4729fa0a90690beedc
Cristian Iosif <cristianiosif@me.com>
2020-05-19 17:51:06 +0300

    Adds github auth through laravel/socialite

--- config/services.php
+++ config/services.php
@@ -33,7 +33,19 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => env('GITHUB_CALLBACK'),
+        'redirect' => env('GITHUB_REDIRECT_URI'),
+    ],
+
+    'twitter' => [
+        'client_id' => env('TWITTER_KEY'),
+        'client_secret' => env('TWITTER_SECRET'),
+        'redirect' => env('TWITTER_REDIRECT_URI')
+    ],
+
+    'facebook' => [
+        'client_id' => env('FACEBOOK_KEY'),
+        'client_secret' => env('FACEBOOK_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT_URI')
     ],
 
 ];
